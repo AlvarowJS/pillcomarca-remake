a2cf6a415294c06ed8f6ba595b72a51dc0390a58
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2023-11-06 15:35:26 -0500

    actuliazando endpoint de produccion

--- src/api/bdMuni.js
+++ src/api/bdMuni.js
@@ -9,7 +9,8 @@ const bdMuni = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api'
 
     // Endpoint VPS
-    baseURL: 'http://161.132.39.123/api'
+    // baseURL: 'http://161.132.39.123/api'
+    baseURL: 'https://sv-yptplguxwm.cloud.elastika.pe/'
 });
 
 
