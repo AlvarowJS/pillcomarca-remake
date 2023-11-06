14fc606cc7eaa25edcf4bd82435f7dbadf65687a
Alvaro Muni <alvaruwiiu@hotmail.com>
2023-11-06 09:00:38 -0500

    endpoints backends

--- src/api/bdMuni.js
+++ src/api/bdMuni.js
@@ -2,8 +2,14 @@
 import axios from 'axios';
 
 const bdMuni = axios.create({
+    // Endpoint de CPANEL
     // baseURL: 'https://backend.munipillcomarca.gob.pe/api'
-    baseURL: 'http://127.0.0.1:8000/api'
+
+    // Endpoint Local
+    // baseURL: 'http://127.0.0.1:8000/api'
+
+    // Endpoint VPS
+    baseURL: 'http://161.132.39.123/api'
 });
 
 
