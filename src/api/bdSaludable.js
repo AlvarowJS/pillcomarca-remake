156cab5a942d610b8a5821c43ba0ed4374ebe34d
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2024-10-02 12:33:51 -0500

    llenado de informacion en la api

--- src/api/bdSaludable.js
+++ src/api/bdSaludable.js
@@ -10,29 +10,49 @@ export const bdSaludable = [
                 fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
             },
             {
-                item: 'Problema Identificado',
-              descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos. ',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Eje Temático',
+                descripcion: 'Segregación en la fuente y cuidado ambiental, mediante la recuperación de puntos críticos ',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028006/1032410-img_4.jpeg'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Síntesis de la Experiencia Exitosa ',
+                descripcion: 'El Programa de Segregación y Recolección selectiva de Residuos Solidos, tiene como objetivo recolectar los residuos orgánicos e inorgánicos del distrito, para luego darles el tratamiento respectivo, a la fecha se viene trabajando con 476 viviendas las cuales entregan sus residuos segregados valorizado en 20.5 Tn de residuos orgánicos mensuales obteniendo el promedio de 1-1.5 Tn compost.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028000/1032410-img_1.jpeg'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Objetivos y Metas',
+                descripcion: '- Mejorar la conciencia ambiental de los habitantes del distrito de Pillco Marca, mediante el Programa de Segregación en la Fuente y Recolección selectiva  e implementar una alimentación saludable, mediante la entrega de hortalizas ricas en hierro, las cuales son producidas en nuestro biohuerto,\n - Disminuir el 40% de residuos sólidos que en la actualidad se esta disponiendo en el botadero controlado de Chilepampa',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7030557/1032642-img_3.jpeg'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Estrategias',
+                descripcion: '- Empadronamiento de nuevos partipantes del Programa de Segregación en la Fuente \n - Con respecto a la valorización de reiduos orgánicos e inorgánicos, se tiene la elaboración y producción compost \n - Cuenta con un vivero y biohuerto.  .',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028004/1032410-img_3.jpeg'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Logros Alcanzados',
+                descripcion: 'Programa de Segregación en la Fuente y recolección selectiva mediante programa "RECICLA".',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028008/1032410-img_5.jpeg'
+            },
+            {
+                item: 'Resultados Obtenidos',
+                descripcion: 'Recuperación de puntos críticos, se realizo con la finalidad de erradicarlos ya que causaban un impacto ambiental visual negativo en el distrito, es por ello que se logró recuperar 6.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7030555/1032642-img_1.jpeg'
+            },
+            {
+                item: 'Lecciones Aprendidas',
+                descripcion: '- Los participantes han aprendido la correcta segregacion de los residuos orgánicos e inorgánicos no aprovechables \n - Obtención de un abono orgánico coo el compost.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028002/1032410-img2.jpeg'
+            },
+            {
+                item: 'Financiamiento',
+                descripcion: 'Recursos de la municipalidad.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7030558/1032642-img_4.jpeg'
+            },
+            {
+                item: 'Responsable de la Experiencia',
+                descripcion: '- Municipalidad Distrital de Pillco Marca: Ing. Diana Plejo Carrillo \n - Gerencia de Medio Ambiente: Ing. Mariela Isabel Ponce Álvarez.\n - Área de Segregación: Ing. German Geremias Primo Cabrera \n - Subgerente de Parques y Jardines: Ing. Rudy Toribio Zevallos.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7030556/1032642-img_2.jpeg'
             },
         ],
         link: '/gestion-integral'
@@ -43,37 +63,100 @@ export const bdSaludable = [
         author: 'de Saneamiento Rural',
         detalles: [
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Problema identificado',
+                descripcion: 'El distrito de Pillco Marca cuenta con 5 centros poblados rurales (S.L. Canchapara, Vicsacocha, Pumacan, Vichaycoto, Rayancatac), con una población total de 17,717 personas aproximadamente, predominando la deficiencia de la sostenibilidad económica de las Juntas Administradoras de los Servicios de Saneamiento (JASS) para la compra de insumos de cloración, actividades de operación y mantenimiento.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028092/1032423-captura1.PNG'
             },
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Eje temático',
+                descripcion: 'Alimentación Y Nutrición Saludable – Recuperación del SAP para brindar agua de calidad a la población rural.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028093/1032423-captura2.PNG'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'Síntesis de la experiencia',
+                descripcion: 'El Área Técnica de Saneamiento de la Municipalidad Distrital de Pillco Marca se encuentra enfocado a brindar agua de calidad en el ámbito rural, encaminados a mejorar la prestación de los servicios de saneamiento, realizando las siguientes actividades de acuerdo a los 3 indicadores que dictan el Programa Nacional de Saneamiento Rural (PNSR):',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026960/1032288-imagen33.PNG'
+            },
+            {
+                item: 'INDICADOR Nº 01: “Porcentaje de viviendas con acceso al servicio de agua de calidad”',
+                descripcion: `Se realizan las siguientes actividades:
+                        ✔ Monitoreo de cloro residual
+                        ✔ Limpieza y desinfección de los SAP
+                        ✔ Calibración del equipo de cloración
+                        ✔ Caracterización del agua
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028095/1032423-captura4.PNG'
+            },
+            {
+                item: 'INDICADOR Nº 02: “Número de sistemas de agua potable en buen estado después del mantenimiento preventivo y/o correctivo”',
+                descripcion: `Se realizan las siguientes actividades:
+                        ✔ Actualización del módulo I,II y III antes y después del mantenimiento.
+                        ✔ Registro y/o actualización del módulo IV.
+                        ✔ Entrega del kit de herramientas al prestador.
+                        ✔ Ejecución del Plan de Mantenimiento preventivo y/o correctivo de los SAP de los Centros Poblados seleccionados según correspondan.
+                        `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029473/1032508-img1.PNG'
+            },
+            {
+                item: 'INDICADOR Nº 03:“Número de viviendas con DSE y/o alcantarillado sanitario que reciben limpieza”',
+                descripcion: `Se realizan las siguientes actividades:
+                    ✔ Empadronamiento y registro del Módulo V de las viviendas del C.P. de Vichaycoto.
+                    ✔ Ejecución de la limpieza de los sistemas DSE del C.P. de Vichaycoto
+                    ✔ Capacitaciones en educación sanitaria.
+                    Se vienen desarrollando las actividades anteriormente mencionadas en los 5 centros poblados rurales (Vichaycoto, San Lorenzo de Canchapara, Pumacan, Vicsacocha y Rayancatac) del distrito, ya que ellos se encuentran en estado malo y regular de acuerdo al último diagnostico situacional.
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028094/1032423-captura3.PNG'
+            },
+            {
+                item: 'Objetivos y metas.',
+                descripcion: `Son los siguientes:
+                    ✔ Garantizar que los sistemas intervenidos se encuentren en buen estado para su funcionamiento continuo.
+                    Contando con los siguientes indicadores para el cumplimiento de meta al 100% en los centros poblados a intervenir:
+                    ✔ Porcentaje de viviendas con acceso al servicio de agua de calidad.
+                    ✔ Numero de sistemas de agua potable en estado bueno después del mantenimiento preventivo y correctivo.
+                    ✔ Número de vivienda de centros poblados con sistemas de disposición sanitaria de excretas y/o alcantarillado sanitario que reciben limpieza.
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029665/1032508-img5.PNG'
+            },
+            {
+                item: 'Estrategias',
+                descripcion: `✔ Capacitaciones a las familias en educación sanitaria y limpieza de UBS.
+                    ✔ Brindar asistencia técnica al consejo directivo de las JASS.
+                    ✔ Capacitaciones a operadores de las JASS.
+                    ✔ Implementación de kit de herramientas para la operación y mantenimiento del sistema de agua potable de las JASS.
+`,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029473/1032508-img1.PNG'
+            },
+            {
+                item: 'Descripción, logro y resultados obtenidos de las experiencias.',
+                descripcion: `✔	La Municipalidad Distrital de Pillco Marca viene trabajando en los 3 indicadores; realizando las actividades del indicador N°01 (monitoreo de cloro residual, limpieza y desinfección del SAP, calibración del equipo de cloración y caracterización del agua) al 75% hasta la fecha; asimismo se ejecutaron 4 planes de mantenimiento del Sistema de Agua Potable SAP en los siguientes Centros Poblados (Rayancatac, Vicsacocha, San Lorenzo de Canchapara, Pumacan y Vichaycoto) representando un 80% de la meta correspondiente al Indicador N° 02. Respecto al indicador N° 03 se culminaron las actividades al 100% (empadronamiento, capacitación y limpieza de UBS).
+                        ✔	Es importante mencionar que la mayoría de los Sistemas de Agua Potable de los centros poblados (Rayancatac, Vicsacocha, San Lorenzo de Canchapara, Pumacan y Vichaycoto), se encontraron en mal estado los siguientes componentes: captación, CRP tipo 6, línea de conducción, reservorio, CRP tipo 7, línea de aducción y red de distribución, debido a la baja economía de la población no realizaban el debido mantenimiento y la cloración del agua por ende la población venia consumiendo agua no clorada causando daños a la salud en niños menores de 5 años y a las personas adultas de la tercera edad.
+                        ✔	Viendo esa problemática se realizó la caracterización del agua de más de 33 parámetros (físicos, químicos, microbiológicos, organolépticos, metales pesados y bacteriológicos) en diferentes centros poblados, para evaluar la calidad del agua que consume la población.
+                        `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029477/1032508-img2.PNG'
+            },
+            {
+                item: 'Lecciones aprendidas',
+                descripcion: 'En lo negativo encontramos el difícil acceso en la carretera para llegar al centro poblado ya que se encuentran con deslizamientos, pendientes, lodo por la constante lluvia que precipita en la zona. La falta de coordinación entre las autoridades de las diferentes organizaciones comunales. La falta de compromiso por una parte de la población en apoyo de la mano de obra no calificada en los trabajos que realizaron articuladamente con la Municipalidad en actividades de mantenimiento del Sistema de Agua Potable.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029479/1032508-img3.PNG'
             },
         ],
         link: '/saneamiento-rural'
     },
     {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=248&fit=crop&auto=format&dpr=2',
+        img: 'https://cdn.www.gob.pe/uploads/document/file/7026772/1032270-img_4-2.jpeg',
         title: 'Creciendo con Amor por Pillco Marca',
         author: 'porque tu Amor es de Hierro',
         detalles: [
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Problema identificado',
+                descripcion: 'En el Distrito de Pillcomarca con mas 39 900,00 habitantes, un aproximado de 4 509 niños menores de 5 años y 2 978 niños menores de 36 meses con un distrito altamente migrante, en condiciones de pobre y pobre extremo, en las partes altas como Potracancha, Killarumi nose cuenta con agua adecuada para el consumo, tampoco cuenta con una buena alimentación ricos en hierro',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026770/1032270-img_2.jpeg'
             },
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Eje Temático',
+                descripcion: 'Agua Segura y alimentacion ricos en hierro',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026769/1032270-img_1-2.jpeg'
             },
             {
                 item: '',
@@ -83,7 +166,7 @@ export const bdSaludable = [
         ],
         link: '/amor-hierro'
     }
-    
+
 
 ]
 export default bdSaludable;
