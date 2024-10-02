785dbea29882eefa01169151c23173720b77abbe
Ruben Picon <piconreyesruben@gmail.com>
2024-10-02 11:09:16 -0500

    se agrego contenido en bdSaludable

--- src/api/bdSaludable.js
+++ src/api/bdSaludable.js
@@ -45,55 +45,80 @@ export const bdSaludable = [
             {
                 item: 'Problema identificado',
                 descripcion: 'El distrito de Pillco Marca cuenta con 5 centros poblados rurales (S.L. Canchapara, Vicsacocha, Pumacan, Vichaycoto, Rayancatac), con una población total de 17,717 personas aproximadamente, predominando la deficiencia de la sostenibilidad económica de las Juntas Administradoras de los Servicios de Saneamiento (JASS) para la compra de insumos de cloración, actividades de operación y mantenimiento.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7024761/1032089-imagen4.PNG'
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028092/1032423-captura1.PNG'
             },
             {
                 item: 'Eje temático',
                 descripcion: 'Alimentación Y Nutrición Saludable – Recuperación del SAP para brindar agua de calidad a la población rural.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026959/1032288-imagen22.PNG'
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028093/1032423-captura2.PNG'
             },
             {
                 item: 'Síntesis de la experiencia',
-<<<<<<< HEAD
-                descripcion: `
-                    El Área Técnica de Saneamiento de la Municipalidad Distrital de Pillco Marca 
-                    se encuentra enfocada en brindar agua de calidad en el ámbito rural, 
-                    encaminados a mejorar la prestación de los servicios de saneamiento, 
-                    realizando las siguientes actividades de acuerdo a los 3 indicadores 
-                    que dicta el Programa Nacional de Saneamiento Rural (PNSR):
-
-                    INDICADOR Nº 01: “Porcentaje de viviendas con acceso al servicio de agua de calidad”
-                    Se realizan las siguientes actividades:
-                    ✔ Monitoreo de cloro residual
-                    ✔ Limpieza y desinfección de los SAP
-                    ✔ Calibración del equipo de cloración
-                    ✔ Caracterización del agua
-
-                    INDICADOR Nº 02: “Número de sistemas de agua potable en buen estado después del mantenimiento preventivo y/o correctivo”
-                    Se realizan las siguientes actividades:
-                    ✔ Actualización del módulo I, II y III antes y después del mantenimiento
-                    ✔ Registro y/o actualización del módulo IV
-                    ✔ Entrega del kit de herramientas al prestador
-                    ✔ Ejecución del Plan de Mantenimiento preventivo y/o correctivo de los SAP de los Centros Poblados seleccionados según correspondan
-
-                    INDICADOR Nº 03: “Número de viviendas con DSE y/o alcantarillado sanitario que reciben limpieza”
-                    Se realizan las siguientes actividades:
-                    ✔ Empadronamiento y registro del Módulo V de las viviendas del C.P. de Vichaycoto
-                    ✔ Ejecución de la limpieza de los sistemas DSE del C.P. de Vichaycoto
-                    ✔ Capacitaciones en educación sanitaria
-
-                    Se vienen desarrollando las actividades anteriormente mencionadas en los 5 centros poblados rurales (Vichaycoto, San Lorenzo de Canchapara, Pumacan, Vicsacocha y Rayancatac) del distrito, ya que ellos se encuentran en estado malo y regular de acuerdo al último diagnóstico situacional.
-                `,
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026960/1032288-imagen33.PNG'
-=======
                 descripcion: 'El Área Técnica de Saneamiento de la Municipalidad Distrital de Pillco Marca se encuentra enfocado a brindar agua de calidad en el ámbito rural, encaminados a mejorar la prestación de los servicios de saneamiento, realizando las siguientes actividades de acuerdo a los 3 indicadores que dictan el Programa Nacional de Saneamiento Rural (PNSR):',
-                fotos: ''
->>>>>>> eacb26ca22d956f5da008ec0b32e9b97b3fc2d91
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026960/1032288-imagen33.PNG'
             },
             {
-                item: 'Problema Identificado',
-                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos.',
-                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
+                item: 'INDICADOR Nº 01: “Porcentaje de viviendas con acceso al servicio de agua de calidad”',
+                descripcion: `Se realizan las siguientes actividades:
+                        ✔ Monitoreo de cloro residual
+                        ✔ Limpieza y desinfección de los SAP
+                        ✔ Calibración del equipo de cloración
+                        ✔ Caracterización del agua
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028095/1032423-captura4.PNG'
+            },
+            {
+                item: 'INDICADOR Nº 02: “Número de sistemas de agua potable en buen estado después del mantenimiento preventivo y/o correctivo”',
+                descripcion: `Se realizan las siguientes actividades:
+                        ✔ Actualización del módulo I,II y III antes y después del mantenimiento.
+                        ✔ Registro y/o actualización del módulo IV.
+                        ✔ Entrega del kit de herramientas al prestador.
+                        ✔ Ejecución del Plan de Mantenimiento preventivo y/o correctivo de los SAP de los Centros Poblados seleccionados según correspondan.
+                        `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029473/1032508-img1.PNG'
+            },
+            {
+                item: 'INDICADOR Nº 03:“Número de viviendas con DSE y/o alcantarillado sanitario que reciben limpieza”',
+                descripcion: `Se realizan las siguientes actividades:
+                    ✔ Empadronamiento y registro del Módulo V de las viviendas del C.P. de Vichaycoto.
+                    ✔ Ejecución de la limpieza de los sistemas DSE del C.P. de Vichaycoto
+                    ✔ Capacitaciones en educación sanitaria.
+                    Se vienen desarrollando las actividades anteriormente mencionadas en los 5 centros poblados rurales (Vichaycoto, San Lorenzo de Canchapara, Pumacan, Vicsacocha y Rayancatac) del distrito, ya que ellos se encuentran en estado malo y regular de acuerdo al último diagnostico situacional.
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7028094/1032423-captura3.PNG'
+            },
+            {
+                item: 'Objetivos y metas.',
+                descripcion: `Son los siguientes:
+                    ✔ Garantizar que los sistemas intervenidos se encuentren en buen estado para su funcionamiento continuo.
+                    Contando con los siguientes indicadores para el cumplimiento de meta al 100% en los centros poblados a intervenir:
+                    ✔ Porcentaje de viviendas con acceso al servicio de agua de calidad.
+                    ✔ Numero de sistemas de agua potable en estado bueno después del mantenimiento preventivo y correctivo.
+                    ✔ Número de vivienda de centros poblados con sistemas de disposición sanitaria de excretas y/o alcantarillado sanitario que reciben limpieza.
+                    `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029665/1032508-img5.PNG'
+            },
+            {
+                item: 'Estrategias',
+                descripcion: `✔ Capacitaciones a las familias en educación sanitaria y limpieza de UBS.
+                    ✔ Brindar asistencia técnica al consejo directivo de las JASS.
+                    ✔ Capacitaciones a operadores de las JASS.
+                    ✔ Implementación de kit de herramientas para la operación y mantenimiento del sistema de agua potable de las JASS.
+`,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029473/1032508-img1.PNG'
+            },
+            {
+                item: 'Descripción, logro y resultados obtenidos de las experiencias.',
+                descripcion: `✔	La Municipalidad Distrital de Pillco Marca viene trabajando en los 3 indicadores; realizando las actividades del indicador N°01 (monitoreo de cloro residual, limpieza y desinfección del SAP, calibración del equipo de cloración y caracterización del agua) al 75% hasta la fecha; asimismo se ejecutaron 4 planes de mantenimiento del Sistema de Agua Potable SAP en los siguientes Centros Poblados (Rayancatac, Vicsacocha, San Lorenzo de Canchapara, Pumacan y Vichaycoto) representando un 80% de la meta correspondiente al Indicador N° 02. Respecto al indicador N° 03 se culminaron las actividades al 100% (empadronamiento, capacitación y limpieza de UBS).
+                        ✔	Es importante mencionar que la mayoría de los Sistemas de Agua Potable de los centros poblados (Rayancatac, Vicsacocha, San Lorenzo de Canchapara, Pumacan y Vichaycoto), se encontraron en mal estado los siguientes componentes: captación, CRP tipo 6, línea de conducción, reservorio, CRP tipo 7, línea de aducción y red de distribución, debido a la baja economía de la población no realizaban el debido mantenimiento y la cloración del agua por ende la población venia consumiendo agua no clorada causando daños a la salud en niños menores de 5 años y a las personas adultas de la tercera edad.
+                        ✔	Viendo esa problemática se realizó la caracterización del agua de más de 33 parámetros (físicos, químicos, microbiológicos, organolépticos, metales pesados y bacteriológicos) en diferentes centros poblados, para evaluar la calidad del agua que consume la población.
+                        `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029477/1032508-img2.PNG'
+            },
+            {
+                item: 'Lecciones aprendidas',
+                descripcion: 'En lo negativo encontramos el difícil acceso en la carretera para llegar al centro poblado ya que se encuentran con deslizamientos, pendientes, lodo por la constante lluvia que precipita en la zona. La falta de coordinación entre las autoridades de las diferentes organizaciones comunales. La falta de compromiso por una parte de la población en apoyo de la mano de obra no calificada en los trabajos que realizaron articuladamente con la Municipalidad en actividades de mantenimiento del Sistema de Agua Potable.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7029479/1032508-img3.PNG'
             },
         ],
         link: '/saneamiento-rural'
