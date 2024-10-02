feb6bc5f6fd690e73993edd17935d302a4ebd77f
Ruben Picon <piconreyesruben@gmail.com>
2024-10-01 21:00:38 -0500

    Se agrego contenido en bdSaludable

--- src/api/bdSaludable.js
+++ src/api/bdSaludable.js
@@ -11,7 +11,7 @@ export const bdSaludable = [
             },
             {
                 item: 'Problema Identificado',
-              descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos. ',
+                descripcion: 'El problema principal que se identifica en el distrito de Pillco Marca, es la mala disposición de los residuos sólidos que se generan en la fuente (viviendas), y la falta de conciencia ambiental de los pobladores del distrito, los cuales generan un impacto ambiental visual negativo en el distrito, proliferación de vectores (moscas y zancudos), los cuales afectan directamente a la salud de los vecinos. ',
                 fotos: 'https://cdn.www.gob.pe/uploads/document/file/7021900/1031874-img-2.jpg'
             },
             {
@@ -48,14 +48,42 @@ export const bdSaludable = [
                 fotos: 'https://cdn.www.gob.pe/uploads/document/file/7024761/1032089-imagen4.PNG'
             },
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Eje temático',
+                descripcion: 'Alimentación Y Nutrición Saludable – Recuperación del SAP para brindar agua de calidad a la población rural.',
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026959/1032288-imagen22.PNG'
             },
             {
-                item: '',
-                descripcion: '',
-                fotos: ''
+                item: 'Síntesis de la experiencia',
+                descripcion: `
+                    El Área Técnica de Saneamiento de la Municipalidad Distrital de Pillco Marca 
+                    se encuentra enfocada en brindar agua de calidad en el ámbito rural, 
+                    encaminados a mejorar la prestación de los servicios de saneamiento, 
+                    realizando las siguientes actividades de acuerdo a los 3 indicadores 
+                    que dicta el Programa Nacional de Saneamiento Rural (PNSR):
+
+                    INDICADOR Nº 01: “Porcentaje de viviendas con acceso al servicio de agua de calidad”
+                    Se realizan las siguientes actividades:
+                    ✔ Monitoreo de cloro residual
+                    ✔ Limpieza y desinfección de los SAP
+                    ✔ Calibración del equipo de cloración
+                    ✔ Caracterización del agua
+
+                    INDICADOR Nº 02: “Número de sistemas de agua potable en buen estado después del mantenimiento preventivo y/o correctivo”
+                    Se realizan las siguientes actividades:
+                    ✔ Actualización del módulo I, II y III antes y después del mantenimiento
+                    ✔ Registro y/o actualización del módulo IV
+                    ✔ Entrega del kit de herramientas al prestador
+                    ✔ Ejecución del Plan de Mantenimiento preventivo y/o correctivo de los SAP de los Centros Poblados seleccionados según correspondan
+
+                    INDICADOR Nº 03: “Número de viviendas con DSE y/o alcantarillado sanitario que reciben limpieza”
+                    Se realizan las siguientes actividades:
+                    ✔ Empadronamiento y registro del Módulo V de las viviendas del C.P. de Vichaycoto
+                    ✔ Ejecución de la limpieza de los sistemas DSE del C.P. de Vichaycoto
+                    ✔ Capacitaciones en educación sanitaria
+
+                    Se vienen desarrollando las actividades anteriormente mencionadas en los 5 centros poblados rurales (Vichaycoto, San Lorenzo de Canchapara, Pumacan, Vicsacocha y Rayancatac) del distrito, ya que ellos se encuentran en estado malo y regular de acuerdo al último diagnóstico situacional.
+                `,
+                fotos: 'https://cdn.www.gob.pe/uploads/document/file/7026960/1032288-imagen33.PNG'
             },
             {
                 item: 'Problema Identificado',
@@ -88,7 +116,7 @@ export const bdSaludable = [
         ],
         link: '/amor-hierro'
     }
-    
+
 
 ]
 export default bdSaludable;
